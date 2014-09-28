ae5e01afba059cf79cfde5e43508279577e68e60
jeroendonkers <jeroen.donkers@maastrichtuniversity.nl>
2014-09-28 22:48:56 +0200

    classic interval and not arithmetics

--- src/musica/symbol/ClassicInterval.scala
+++ src/musica/symbol/ClassicInterval.scala
@@ -5,25 +5,101 @@ class ClassicInterval(val step: Int, val dev: Int = 0) {
   // dev = deviation from basic size
   val normstep = step.abs % 7
   val size =  ClassicInterval.basicsize(normstep) + 12 * (step.abs/7) + dev
-  val octaves = normstep/7
+  val octaves = step.abs/7
   val canbepure = ClassicInterval.canbepure(normstep)
   
+   def normalize(): ClassicInterval = {
+    new ClassicInterval(if (step<0) 7-normstep else normstep, dev)    
+  }
+    
+  def +(that: ClassicInterval) = 
+     new ClassicInterval(step + that.step, size + that.size - ClassicInterval(step + that.step).size)
+  def -(that: ClassicInterval) = 
+     new ClassicInterval(step - that.step, size - that.size - ClassicInterval(step - that.step).size)
+  def *(that: Int) = 
+     new ClassicInterval(step * that, size * that - ClassicInterval(step * that).size)
+  def unary_- :ClassicInterval = ClassicInterval.Prime - this
   
+  def invert(): ClassicInterval = ClassicInterval.Octave - this
   
+  override def equals(that: Any): Boolean = {
+  that match {
+       case that: ClassicInterval => (this.step == that.step) && (this.dev == that.dev)
+       case _ => false
+     }
+   }
   
+   override def toString(): String = {
+     val basname = if (step.abs<15) ClassicInterval.intervalName(step.abs) else
+                        ClassicInterval.intervalName(normstep)+" "+(octaves*8)+"va"
+     val aspect = if (ClassicInterval.canbepure(normstep)) dev match {
+       case 0 => "Pure" 
+       case 1 => "Augmented"
+       case -1 => "Diminished" 
+       case _ => "Irregular ("+dev+")"  
+     } else dev match {
+       case 0 => "Major" 
+       case 1 => "Augmented"
+       case -1 => "Minor"
+       case -2 => "Dimished"
+       case _ => "Irregular ("+dev+")"  
+     }
+     val sign = if (step<0) " down" else ""
+     aspect + " "+ basname + sign                  
+   }
+   
 }
 
 object ClassicInterval {
   val intervalName =  Array ("Prime", "Second","Third", "Fourth", 
-      "Fifth", "Sixth", "Seventh", "Octave", "Ninth")
+      "Fifth", "Sixth", "Seventh", "Octave", "Ninth", "Tenth", "Eleventh", "Twelfth", "Thirteenth", "Fourteenth", "Fifteenth")
+  
   val canbepure = Array(true, false, false, true, true, false, false)
   val basicsize = Array(0,2,4,5,7,9,11)
   
+  def apply(step: Int, dev: Int = 0) = new ClassicInterval(step,dev)
+  
+  def Prime = ClassicInterval(0)
+  def MinorSecond = ClassicInterval(1)
+  def MajorSecond = ClassicInterval(1,-1)
+  def MinorThird = ClassicInterval(2,-1)
+  def MajorThird = ClassicInterval(2)
+  def Fourth = ClassicInterval(3)
+  def Fifth = ClassicInterval(4)
+  def MinorSixth = ClassicInterval(5,-1)
+  def MajorSixth = ClassicInterval(0)
+  def MinorSeventh = ClassicInterval(6,-1)
+  def MajorSeventh = ClassicInterval(6)
+  def Octave = ClassicInterval(7)
+ 
 }
 
-class ClassicNote(stp: Int, val dev: Int, val octave: Int = 0) {
+class ClassicNote(stp: Int, val dev: Int = 0, val octave: Int = 0) {
   val step = if (stp<0 || stp>7) 0 else stp 
   val chr = ClassicNote.NotePos(step) + dev + octave*12
+  
+  def +(that: ClassicInterval): ClassicNote = {
+     if (that.step <0) this - (-that) else {   
+    
+     val newstep = (step + that.step) % 7
+     val newoctave = octave + (step + that.step) / 7
+     
+     val newdev = chr + that.size - ClassicNote(newstep,0,newoctave).chr
+     new ClassicNote(newstep,newdev, newoctave)
+     }
+  }
+  
+  def -(that: ClassicInterval): ClassicNote =  {
+    if (that.step <0) this + (-that) else {
+      
+      val ns = that.step % 7
+      val newstep = (step - ns + (if (ns>step) 7 else 0)) % 7
+      val newoctave = octave + (step - that.step) / 7 - (if (that.step>step) 1 else 0)
+      val newdev = (chr - that.size) - (ClassicNote(newstep,0,newoctave).chr )
+      new ClassicNote(newstep,newdev, newoctave)
+     }
+  }
+  
   override def toString(): String = {
     ClassicNote.NoteName(step) + 
       (if (dev>0) ("#"*dev) else (if (dev<0) ("b" * (-dev)) else "")) +
@@ -35,7 +111,7 @@ object ClassicNote {
   val NoteName = Array('C', 'D', 'E', 'F', 'G', 'A', 'B')
   val NotePos = Array(0,2,4,5,7,9,11)
   
-  def apply(stp: Int, dev: Int, octave: Int = 0) = 
+  def apply(stp: Int, dev: Int = 0, octave: Int = 0) = 
     new ClassicNote(stp,dev,octave)
   
 }
--- test/musica/symbol/ClassicIntervalTest.scala
+++ test/musica/symbol/ClassicIntervalTest.scala
@@ -7,51 +7,143 @@ class ClassicIntervalTest {
 
   @Test def ClassicNoteStringTest() {
     val c = new ClassicNote(2,1,0)
-    assertEquals(c.toString(), "E#")
+    assertEquals("E#", c.toString())
     val d = new ClassicNote(8,-3,2)
-    assertEquals(d.toString(), "Cbbb+2")
+    assertEquals("Cbbb+2",d.toString())
     val e = new ClassicNote(4,+2,-3)
-    assertEquals(e.toString(), "G##-3")
+    assertEquals("G##-3",e.toString())
   }
 
   @Test def ClassicNoteChrTest() {
     val c = ClassicNote(2,1,0)
-    assertEquals(c.chr, 5)
+    assertEquals(5, c.chr)
     val d = ClassicNote(8,-3,2)
-    assertEquals(d.chr, 21)
+    assertEquals(21, d.chr)
     val e = ClassicNote(4,+2,-3)
-    assertEquals(e.chr, -27)
+    assertEquals(-27, e.chr)
   }
   
   @Test def ClassicIntervalDevTest1() {
     val a = new ClassicInterval(0,1)
-    assertEquals(a.size,1)        
+    assertEquals(1, a.size)        
   }
   
   @Test def ClassicIntervalDevTest2() {
     val b = new ClassicInterval(4)
-    assertEquals(b.size,7)
+    assertEquals(7, b.size)
   }  
    
   @Test def ClassicIntervalDevTest3() {
     val c = new ClassicInterval(1,-1)
-    assertEquals(c.size,1)
+    assertEquals(1, c.size)
   }
   
   @Test def ClassicIntervalDevTest4() {
     val d = new ClassicInterval(8,0)
-    assertEquals(d.size,14)
+    assertEquals(14, d.size)
   }
   
   @Test def ClassicIntervalDevTest5() {
     val d = new ClassicInterval(-1,0)
-    assertEquals(d.size,2)
+    assertEquals(2, d.size)
   }
   
   @Test def ClassicIntervalDevTest6() {
     val d = new ClassicInterval(-8,1)
-    assertEquals(d.size,15)
+    assertEquals(15, d.size)
   }
-    
   
+  @Test def ClassicIntervalAddTest() {
+    val a = new ClassicInterval(2,0)
+    val b = new ClassicInterval(2,-1)
+    val c = new ClassicInterval(4,0)
+    assertEquals(c, a+b)
+  }
+  
+   @Test def ClassicIntervalAddTest1() {
+     assertEquals(ClassicInterval.Fifth, ClassicInterval.MinorThird+ClassicInterval.MajorThird)
+  }
+
+  @Test def ClassicIntervalAddTest2() {
+     assertEquals(ClassicInterval.Fifth, ClassicInterval.Prime+ClassicInterval.Fifth)
+  }
+ 
+   
+  @Test def ClassicIntervalSubTest1() {
+     assertEquals(ClassicInterval.MinorThird,ClassicInterval.Fifth-ClassicInterval.MajorThird)
+  }
+
+  @Test def ClassicIntervalMulTest1() {
+     val a = ClassicInterval.MajorThird
+     val b = ClassicInterval(4,1) // augmented fifth
+     assertEquals(b, a*2)
+  }
+  
+  @Test def ClassicIntervalNegTest1() {
+     val a = ClassicInterval.Fifth
+     val b = -ClassicInterval.MinorThird 
+     val c = ClassicInterval.MajorThird
+     val d = a+b
+     assertEquals(c, d)
+  }
+  
+  @Test def ClassicIntervalNegNegTest1() {
+     val a = ClassicInterval(6,-1)
+     val b = -a 
+     val c = -b
+     assertEquals(c, a)
+  }
+  
+  
+  @Test def ClassicIntervalStrTest1() {
+     val a = ClassicInterval(12,-1)
+     assertEquals("Minor Thirteenth", a.toString())
+  }
+  
+    @Test def ClassicIntervalStrTest2() {
+     val a = ClassicInterval(11,-1)
+     assertEquals("Diminished Twelfth", a.toString())
+    }
+
+    @Test def ClassicIntervalStrTest3() {
+     val a = ClassicInterval(33,-4)
+     assertEquals("Irregular (-4) Sixth 32va", a.toString())
+    }
+
+    @Test def ClassicIntervalStrTest4() {
+     val a = ClassicInterval(-2)
+     assertEquals("Major Third down", a.toString())
+    }
+    
+    @Test def ClassicIntervalNormTest1() {
+     val a = ClassicInterval(11,-1).normalize()
+     assertEquals("Diminished Fifth", a.toString())
+    }
+    
+    @Test def ClassicIntervalInvertTest1() {
+     val a = ClassicInterval(1,0).invert()
+     assertEquals("Minor Seventh", a.toString())
+    }
+    
+    @Test def NotePlusIntervaltest1() {
+      val a = ClassicNote(0)
+      val b = ClassicInterval(1)
+      val c = a+b
+      assertEquals("D",c.toString())
+    }
+    
+    @Test def NoteMinusIntervaltest1() {
+      val a = ClassicNote(4)
+      val b = ClassicInterval(6)
+      val c = a-b
+      assertEquals("Ab-1",c.toString())
+    }
+    
+     @Test def NoteMinusIntervaltest2() {
+      val a = ClassicNote(4)
+      val b = ClassicInterval(6)
+      val c = a + (-b)
+      assertEquals("Ab-1",c.toString())
+    }
+    
 }
